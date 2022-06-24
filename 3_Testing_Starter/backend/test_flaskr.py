c1e14659002bda4fa37e7238a225d18582a8fd17
Ananya Rana <ananyarana2920@gmail.com>
2022-06-25 01:23:16 +0530

    Update test_flaskr.py

--- 3_Testing_Starter/backend/test_flaskr.py
+++ 3_Testing_Starter/backend/test_flaskr.py
@@ -16,11 +16,15 @@ class BookTestCase(unittest.TestCase):
         self.client = self.app.test_client
         self.database_name = "bookshelf_test"
         self.database_path = "postgresql://{}:{}@{}/{}".format(
-            "student", "student", "localhost:5432", self.database_name
+            "student", "anniebebo47", "localhost:5432", self.database_name
         )
         setup_db(self.app, self.database_path)
 
-        self.new_book = {"title": "Anansi Boys", "author": "Neil Gaiman", "rating": 5}
+        self.new_book = {
+            "title": "Anansi Boys",
+            "author": "Neil Gaiman",
+            "rating": 5
+        }
 
         # binds the app to the current context
         with self.app.app_context():
@@ -33,13 +37,32 @@ class BookTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
+    def test_retrieve_paginated_books(self):
+        res = self.client().get("/books")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['books'])
+        self.assertEqual(len(data['total_books']))
+    
+    def test_404_request_beyond_valid_page(self):
+        res = self.client().get("/books?page=1000")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
+    #def test_update_book_rating(self):
+
+
 
 # @TODO: Write at least two tests for each endpoint - one each for success and error behavior.
 #        You can feel free to write additional tests for nuanced functionality,
 #        Such as adding a book without a rating, etc.
 #        Since there are four routes currently, you should have at least eight tests.
 # Optional: Update the book information in setUp to make the test database your own!
-
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
